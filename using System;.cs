9649419f5791ea202d1103fc21c2c0a1f4d1a7a3
Agarwal <2406697@cognizant.com>
2025-06-30 17:11:19 +0530

    new files added

---
+++ using System;.cs
@@ -0,0 +1,14 @@
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            int a;
+            Console.ReadLine(a);
+            Console.WriteLine(a+1);
+        }
+    }
+}
